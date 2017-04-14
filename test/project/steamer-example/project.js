99134e55f4f4513db26e2f58f6c99f520b2ad9ae
lcxfs1991@gmail.com <lcxfs1991@gmail.com>
2017-04-15 02:10:16 +0800

    update

---
+++ test/project/steamer-example/project.js
@@ -0,0 +1,32 @@
+module.exports = {
+    "name": "steamer-example",
+    "offline": {
+        "publish": true,
+        "compatible": 0,
+        "bid": "2414",
+        "qversionfrom": "0",
+        "qversionto": "0",
+        "platform": [
+            2,
+            3
+        ],
+        "loadmode": 2,
+        "frequency": 1,
+        "verifyType": 0,
+        "expire_time": 1577836800000,
+        "cdn": "defaultCDN",
+        "note": "",
+        "gray": true,
+        "uins": [],
+        "verifyFilterRegex": "html|js|png"
+    },
+    "distConfig": {
+        "buildCommand": {
+            "command": "npm",
+            "args": [
+                "run",
+                "dist"
+            ]
+        }
+    }
+}
